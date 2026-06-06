164acd011f89d0680c1f505c76ac505c6f208f60
TwoCupsOfT <cmy7795@gmail.com>
2026-06-06 13:17:57 +0900

    입력 함수 수정2

--- 1220.py
+++ 1220.py
@@ -4,10 +4,12 @@ def star_input():
     try:
         res = input('중심 항성의 질량(kg), 추가하실 공전 행성의 개수를\n공백으로 구분하여 차례로 입력하세요\n>_ ').split()
         mass,N = float(res[0]), int(res[1])
+
         if mass <= 0 or N <= 0:
             raise ValueError('항성의 질량 및 행성의 개수는 0보다 커야 합니다.')
 
         return mass,N
+    
     except (ValueError, IndexError) as e:
         print(f'\n입력 형식이 잘못되었습니다! 다시 입력해주세요. \n{e}\n')
         return star_input()
@@ -19,13 +21,18 @@ def input_values():
     for _ in range(N):
         while True:
             try:
-                name,x0,y0,v0 = input('\n공전 행성의 \n이름,근일점 x좌표(AU),근일점 y좌표(AU),근일점 속력(km/s)\n을 공백으로 구분하여 차례로 입력하세요\n>_ ').split()
-                planet_info[name] = {'peri_pos':[float(x0),float(y0)],
-                                     'peri_dist':(x0+y0)**0.5,
-                                     'peri_speed':float(v0)}
+                name,x0,y0,v0 = input('\n공전 행성의 \n이름,근일점 x좌표(AU),근일점 y좌표(AU),근일점 속력(km/s)\n을 공백으로 구분하여 차례로 입력하세요\n(중심 항성의 좌표는 (0,0)입니다.)\n>_ ').split()
+                x0,y0,v0 = map(float,(x0,y0,v0))
+
                 if (x0 == 0 and y0 == 0) or v0 <= 0:
                     raise ValueError('근일점 위치는 항성의 위치 (0,0)이 될 수 없으며, 행성의 속력은 0보다 커야 합니다.')
+                
+                planet_info[name] = {'peri_pos':[x0,y0],
+                                     'peri_dist':(x0*x0+y0*y0)**0.5,
+                                     'peri_speed':v0}
+
                 break
+
             except ValueError as e:
                 print(f'\n입력 형식이 잘못되었습니다! 다시 입력해주세요. \n{e}\n')
 
