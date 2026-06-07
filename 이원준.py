d8746659a9496e79fff096b9304944535fb345e0
Renjwi <yuanjun351@gmail.com>
2026-06-07 21:44:20 +0900

    이심률 추가 및 주기 수정

--- 이원준.py
+++ 이원준.py
@@ -2,12 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-def orbital_period(Longer_Radius,Main_Mass):
-    G = 6.6743*10**-11
-    period = 2*math.pi * (Longer_Radius**3 / Main_Mass)**0.5
-    return period
+def orbital_period(Longer_Radius,Main_Mass,Planet):
+    G = 0.99893960704 #AU, yr, Mo기준
+    Period = 2*math.pi * (Longer_Radius**3 /G/Main_Mass)**0.5
+    Planet['period']=Period
 
-def cal_ecc(Peri_Distance, Longer_Radius):
-    Eccentricity = 1 - Peri_Distance/Longer_Radius
+def cal_ecc(Planet, Longer_Radius):
+    Distance = Planet['peri_dist']
+    Eccentricity = 1 - Distance/Longer_Radius
+    Planet['eccentricity']=Eccentricity
     return Eccentricity
 
