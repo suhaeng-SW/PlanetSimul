26628daa5f8b4c4867098f041dabf0b07fc5bb90
TwoCupsOfT <cmy7795@gmail.com>
2026-06-09 21:19:31 +0900

    main 다듬기

--- team3_main.py
+++ team3_main.py
@@ -1,6 +1,12 @@
 from 최무율 import *
 from 이원준 import *
-#from 김민수 import *
+from 김민수 import *
+
+print('\n'*10)
+print('=========================================')
+print('케플러 법칙을 적용한 행성 궤도 계산기')
+print('1204, 1215, 1220')
+print('=========================================\n')
 
 M,planet_info = input_values()
 plt.figure(figsize=(8,8))
@@ -10,10 +16,14 @@ for name,planet in planet_info.items():
     a,b = cal_oval(M,planet)
     orbital_period(a,M,planet)
     drawing_graph(a,b,planet,name)
-    planet['speed_av']=-1 # 임시처리
+    cal_speed_av(planet,a,b)
 plt.axis('equal')
 plt.legend()
 plt.show()
+
+print_final(planet_info)
+input('\n계속하려면 아무 키나 입력하세요.')
+print('\n'*10)
 
 while True:
     order = input_manual()
@@ -22,7 +32,7 @@ while True:
         planet_adding = input_values(first=False,planet_info=planet_info)
         a,b = cal_oval(M,planet_info[planet_adding])
         orbital_period(a,M,planet_info[planet_adding])
-        planet_info[planet_adding]['speed_av']=-1 # 임시처리
+        cal_speed_av(planet_info[planet_adding],a,b)
     elif order == 2:
         remove_planet(planet_info)
     elif order == 3:
